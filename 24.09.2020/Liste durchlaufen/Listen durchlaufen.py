62dee7b0acadde4dbd1c7d5ccd07cabf66f0b691
Finn Palzer <palzerfinn@gmail.com>
2020-10-01 09:40:30 +0200

    Update 01.10.2020

---
+++ 24.09.2020/Liste durchlaufen/Listen durchlaufen.py
@@ -0,0 +1,13 @@
+adressen = ['alla@gmx.de','sudo@gmx.de','buffpiffpaf@gmx.de','boing@gmx.de','juhuuuu@gmx.de','ceeeee@gmx.de','beeee@gmx.de','xy@gmx.de','x@gmx.de']
+
+#1
+i=0
+
+while(i<len(adressen)):
+    print(adressen[i])
+    i = i+1
+
+#2
+for a in adressen:
+    print(a)
+    print (a[0])
