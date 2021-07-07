f7b999c811c16ee5934467eaa902a5ec6304c69f
Finn Palzer <palzerfinn@gmail.com>
2021-07-07 14:45:35 +0200

    juhuu

--- 07.07.21/Listen-Übungen.py
+++ 07.07.21/Listen-Übungen.py
@@ -55,7 +55,20 @@ def lotto_zusatz(x):
             y = randint(0,49)
         L = L + [y]
     print(L)
+    #Sortierung(gemogelt):
+    sortiert = sorted(L)
+    print(sortiert)
 
+    #Sortierung so vie es der Lehrer will:
+    """#Bubblesort
+    for z in range(len(y)):
+        for i in range(len(y)-1):
+            if (y[i] > y[i+1]):
+                y[i], y[i+1] = y[i+1], y[i]
+            else:
+                pass
+
+"""
 
 
 
